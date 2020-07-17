b262b92ca9371ed53a7e37422b73063e76c9a88a
VicCoro <corona.victoria@hotmail.com>
2020-07-17 10:53:46 -0700

    sent a GET request to api to update App's state

--- client/components/app.jsx
+++ client/components/app.jsx
@@ -9,11 +9,24 @@ class App extends React.Component {
     };
   }
 
+  componentDidMount() {
+    fetch('/api/grades', {
+      method: 'GET'
+    })
+      .then(response => response.json()).then(data => {
+        this.setState({ grades: data });
+      })
+      .catch(error => {
+        console.error(error.message);
+      });
+  }
+
   render() {
     return (
       <PageTitle text="Student Grade Table"/>
     );
   }
+
 }
 
 export default App;
